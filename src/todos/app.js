df6fa645f635cc6e2278b8f1e3bf35335303a9f3
Tobias Vega <tobivega1310@gmail.com>
2026-09-01 21:49:13 -0300

    feat: implementar función para mostrar todos en la aplicación

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,15 +1,27 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store'
+import { renderTodos } from './use-cases';
+
+const ElementsIDs = {
+  TodoList: '.todo-list',
+};
 /**
  * 
  * @param {String} elementId 
  */
 export const App = (elementId) => {
 
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(ElementsIDs.TodoList, todos);
+  }
+
   // Cuando la función App() se llama
   (() => {
     const app = document.createElement('div');
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    displayTodos();
   })();
 
 }
