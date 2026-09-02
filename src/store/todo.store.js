88657639476713542c02cdf71452b21b53e80633
Tobias Vega <tobivega1310@gmail.com>
2026-09-01 21:17:57 -0300

    feat: definir funciones de gestión de todos en el store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,57 @@ const initStore = () => {
   console.log('InitStore');
 }
 
+const loadStore = () => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId
+ */
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+}
+
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 }
