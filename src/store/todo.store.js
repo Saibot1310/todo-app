3a0cc8477cdedb3eba620a4782ac82c684e412b5
Tobias Vega <tobivega1310@gmail.com>
2026-09-02 11:28:56 -0300

    feat: implementar local storage para persistenica de todos

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -18,12 +18,20 @@ const state = {
 };
 
 const initStore = () => {
-  console.log(state); 
+  loadStore()
   console.log('InitStore');
 }
 
 const loadStore = () => {
-  throw new Error('Not implemented');
+  if (!localStorage.getItem('state')) return;
+
+  const { todos = [], filter = Filters.All } = JSON.parse(localStorage.getItem('state'));
+  state.todos = todos;
+  state.filter = filter;
+}
+
+const saveStateToLocalStorage = () => {
+  localStorage.setItem('state', JSON.stringify(state));
 }
 
 /**
@@ -55,6 +63,7 @@ const addTodo = (description) => {
   if (!description) throw new Error('Description is required');
 
   state.todos.push(new Todo(description));
+  saveStateToLocalStorage();
 }
 
 /**
@@ -69,6 +78,8 @@ const toggleTodo = (todoId) => {
 
     return todo;
   });
+
+  saveStateToLocalStorage();
 }
 
 /**
@@ -77,10 +88,12 @@ const toggleTodo = (todoId) => {
  */
 const deleteTodo = (todoId) => {
   state.todos = state.todos.filter(todo => todo.id !== todoId);
+  saveStateToLocalStorage();
 }
 
 const deleteCompleted = () => {
   state.todos = state.todos.filter(todo => todo.done);
+  saveStateToLocalStorage();
 }
 
 /**
@@ -89,6 +102,7 @@ const deleteCompleted = () => {
  */
 const setFilter = (newFilter = Filters.All) => {
   state.filter = newFilter;
+  saveStateToLocalStorage();
 }
 
 const getCurrentFilter = () => {
